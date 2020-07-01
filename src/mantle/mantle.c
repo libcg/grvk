d6324576f8bc88940b9f3b08dc4e23fb3688be71
Clément Guérin <libcg@protonmail.com>
2020-07-01 16:44:56 -0700

    move queue functions to mantle_queue.c

--- src/mantle/mantle.c
+++ src/mantle/mantle.c
@@ -140,61 +140,6 @@ static VkPipelineLayout getVkPipelineLayout(
     return layout;
 }
 
-// Queue Functions
-
-GR_RESULT grGetDeviceQueue(
-    GR_DEVICE device,
-    GR_ENUM queueType,
-    GR_UINT queueId,
-    GR_QUEUE* pQueue)
-{
-    VkDevice vkDevice = (VkDevice)device;
-    VkQueue vkQueue = VK_NULL_HANDLE;
-
-    vkGetDeviceQueue(vkDevice, getVkQueueFamilyIndex(queueType), queueId, &vkQueue);
-
-    *pQueue = (GR_QUEUE)vkQueue;
-    return GR_SUCCESS;
-}
-
-GR_RESULT grQueueSubmit(
-    GR_QUEUE queue,
-    GR_UINT cmdBufferCount,
-    const GR_CMD_BUFFER* pCmdBuffers,
-    GR_UINT memRefCount,
-    const GR_MEMORY_REF* pMemRefs,
-    GR_FENCE fence)
-{
-    VkQueue vkQueue = (VkQueue)queue;
-    VkFence vkFence = (VkFence)fence;
-
-    VkCommandBuffer* vkCommandBuffers = malloc(sizeof(VkCommandBuffer) * cmdBufferCount);
-    for (int i = 0; i < cmdBufferCount; i++) {
-        vkCommandBuffers[i] = (VkCommandBuffer)pCmdBuffers[i];
-    }
-
-    VkSubmitInfo submitInfo = {
-        .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-        .pNext = NULL,
-        .waitSemaphoreCount = 0,
-        .pWaitSemaphores = NULL,
-        .pWaitDstStageMask = NULL,
-        .commandBufferCount = cmdBufferCount,
-        .pCommandBuffers = vkCommandBuffers,
-        .signalSemaphoreCount = 0,
-        .pSignalSemaphores = NULL,
-    };
-
-    if (vkQueueSubmit(vkQueue, 1, &submitInfo, vkFence) != VK_SUCCESS) {
-        printf("%s: vkQueueSubmit failed\n", __func__);
-        return GR_ERROR_OUT_OF_MEMORY;
-    }
-
-    free(vkCommandBuffers);
-
-    return GR_SUCCESS;
-}
-
 // Image View Functions
 
 GR_RESULT grCreateColorTargetView(
