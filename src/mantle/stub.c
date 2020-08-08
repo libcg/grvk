c96bc37b4f63c17999cb2bf5846eca5ed47956c9
Clément Guérin <libcg@protonmail.com>
2020-08-08 14:51:33 -0700

    mantle: stub: use logger

--- src/mantle/stub.c
+++ src/mantle/stub.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <string.h>
 #include "mantle/mantle.h"
 #include "mantle/mantleDbg.h"
 #include "mantle/mantleWsiWinExt.h"
+#include "logger.h"
 
 // Initialization and Device Functions
 
@@ -12,14 +12,14 @@ GR_RESULT grGetGpuInfo(
     GR_SIZE* pDataSize,
     GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grDestroyDevice(
     GR_DEVICE device)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -29,7 +29,7 @@ GR_RESULT grGetExtensionSupport(
     GR_PHYSICAL_GPU gpu,
     const GR_CHAR* pExtName)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 
     if (strcmp(pExtName, "GR_WSI_WINDOWS") == 0) {
         return GR_SUCCESS;
@@ -43,14 +43,14 @@ GR_RESULT grGetExtensionSupport(
 GR_RESULT grQueueWaitIdle(
     GR_QUEUE queue)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grDeviceWaitIdle(
     GR_DEVICE device)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -59,7 +59,7 @@ GR_RESULT grQueueSetGlobalMemReferences(
     GR_UINT memRefCount,
     const GR_MEMORY_REF* pMemRefs)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -68,7 +68,7 @@ GR_RESULT grQueueSetGlobalMemReferences(
 GR_RESULT grFreeMemory(
     GR_GPU_MEMORY mem)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -76,7 +76,7 @@ GR_RESULT grSetMemoryPriority(
     GR_GPU_MEMORY mem,
     GR_ENUM priority)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -89,7 +89,7 @@ GR_RESULT grRemapVirtualMemoryPages(
     GR_UINT postSignalSemaphoreCount,
     const GR_QUEUE_SEMAPHORE* pPostSignalSemaphores)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -99,7 +99,7 @@ GR_RESULT grPinSystemMemory(
     GR_SIZE memSize,
     GR_GPU_MEMORY* pMem)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -108,7 +108,7 @@ GR_RESULT grPinSystemMemory(
 GR_RESULT grDestroyObject(
     GR_OBJECT object)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -121,7 +121,7 @@ GR_RESULT grGetFormatInfo(
     GR_SIZE* pDataSize,
     GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -130,7 +130,7 @@ GR_RESULT grCreateImage(
     const GR_IMAGE_CREATE_INFO* pCreateInfo,
     GR_IMAGE* pImage)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -141,7 +141,7 @@ GR_RESULT grGetImageSubresourceInfo(
     GR_SIZE* pDataSize,
     GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -150,7 +150,7 @@ GR_RESULT grCreateSampler(
     const GR_SAMPLER_CREATE_INFO* pCreateInfo,
     GR_SAMPLER* pSampler)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -161,7 +161,7 @@ GR_RESULT grCreateImageView(
     const GR_IMAGE_VIEW_CREATE_INFO* pCreateInfo,
     GR_IMAGE_VIEW* pView)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -170,7 +170,7 @@ GR_RESULT grCreateDepthStencilView(
     const GR_DEPTH_STENCIL_VIEW_CREATE_INFO* pCreateInfo,
     GR_DEPTH_STENCIL_VIEW* pView)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -181,7 +181,7 @@ GR_RESULT grCreateComputePipeline(
     const GR_COMPUTE_PIPELINE_CREATE_INFO* pCreateInfo,
     GR_PIPELINE* pPipeline)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -190,7 +190,7 @@ GR_RESULT grStorePipeline(
     GR_SIZE* pDataSize,
     GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -200,7 +200,7 @@ GR_RESULT grLoadPipeline(
     const GR_VOID* pData,
     GR_PIPELINE* pPipeline)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -211,7 +211,7 @@ GR_RESULT grCreateQueryPool(
     const GR_QUERY_POOL_CREATE_INFO* pCreateInfo,
     GR_QUERY_POOL* pQueryPool)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -222,14 +222,14 @@ GR_RESULT grGetQueryPoolResults(
     GR_SIZE* pDataSize,
     GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grGetFenceStatus(
     GR_FENCE fence)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -238,7 +238,7 @@ GR_RESULT grCreateQueueSemaphore(
     const GR_QUEUE_SEMAPHORE_CREATE_INFO* pCreateInfo,
     GR_QUEUE_SEMAPHORE* pSemaphore)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -246,7 +246,7 @@ GR_RESULT grSignalQueueSemaphore(
     GR_QUEUE queue,
     GR_QUEUE_SEMAPHORE semaphore)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -254,7 +254,7 @@ GR_RESULT grWaitQueueSemaphore(
     GR_QUEUE queue,
     GR_QUEUE_SEMAPHORE semaphore)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -263,28 +263,28 @@ GR_RESULT grCreateEvent(
     const GR_EVENT_CREATE_INFO* pCreateInfo,
     GR_EVENT* pEvent)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grGetEventStatus(
     GR_EVENT event)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grSetEvent(
     GR_EVENT event)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grResetEvent(
     GR_EVENT event)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -295,7 +295,7 @@ GR_RESULT grGetMultiGpuCompatibility(
     GR_PHYSICAL_GPU gpu1,
     GR_GPU_COMPATIBILITY_INFO* pInfo)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -304,7 +304,7 @@ GR_RESULT grOpenSharedMemory(
     const GR_MEMORY_OPEN_INFO* pOpenInfo,
     GR_GPU_MEMORY* pMem)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -313,7 +313,7 @@ GR_RESULT grOpenSharedQueueSemaphore(
     const GR_QUEUE_SEMAPHORE_OPEN_INFO* pOpenInfo,
     GR_QUEUE_SEMAPHORE* pSemaphore)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -322,7 +322,7 @@ GR_RESULT grOpenPeerMemory(
     const GR_PEER_MEMORY_OPEN_INFO* pOpenInfo,
     GR_GPU_MEMORY* pMem)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -332,7 +332,7 @@ GR_RESULT grOpenPeerImage(
     GR_IMAGE* pImage,
     GR_GPU_MEMORY* pMem)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -341,7 +341,7 @@ GR_RESULT grOpenPeerImage(
 GR_RESULT grResetCommandBuffer(
     GR_CMD_BUFFER cmdBuffer)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -352,7 +352,7 @@ GR_VOID grCmdBindDynamicMemoryView(
     GR_ENUM pipelineBindPoint,
     const GR_MEMORY_VIEW_ATTACH_INFO* pMemView)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdBindIndexData(
@@ -361,7 +361,7 @@ GR_VOID grCmdBindIndexData(
     GR_GPU_SIZE offset,
     GR_ENUM indexType)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdDrawIndirect(
@@ -369,7 +369,7 @@ GR_VOID grCmdDrawIndirect(
     GR_GPU_MEMORY mem,
     GR_GPU_SIZE offset)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdDrawIndexedIndirect(
@@ -377,7 +377,7 @@ GR_VOID grCmdDrawIndexedIndirect(
     GR_GPU_MEMORY mem,
     GR_GPU_SIZE offset)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdDispatch(
@@ -386,7 +386,7 @@ GR_VOID grCmdDispatch(
     GR_UINT y,
     GR_UINT z)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdDispatchIndirect(
@@ -394,7 +394,7 @@ GR_VOID grCmdDispatchIndirect(
     GR_GPU_MEMORY mem,
     GR_GPU_SIZE offset)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdCopyMemory(
@@ -404,7 +404,7 @@ GR_VOID grCmdCopyMemory(
     GR_UINT regionCount,
     const GR_MEMORY_COPY* pRegions)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdCopyImage(
@@ -414,7 +414,7 @@ GR_VOID grCmdCopyImage(
     GR_UINT regionCount,
     const GR_IMAGE_COPY* pRegions)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdCopyMemoryToImage(
@@ -424,7 +424,7 @@ GR_VOID grCmdCopyMemoryToImage(
     GR_UINT regionCount,
     const GR_MEMORY_IMAGE_COPY* pRegions)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdCopyImageToMemory(
@@ -434,7 +434,7 @@ GR_VOID grCmdCopyImageToMemory(
     GR_UINT regionCount,
     const GR_MEMORY_IMAGE_COPY* pRegions)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdResolveImage(
@@ -444,7 +444,7 @@ GR_VOID grCmdResolveImage(
     GR_UINT regionCount,
     const GR_IMAGE_RESOLVE* pRegions)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdCloneImageData(
@@ -454,7 +454,7 @@ GR_VOID grCmdCloneImageData(
     GR_IMAGE destImage,
     GR_ENUM destImageState)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdUpdateMemory(
@@ -464,7 +464,7 @@ GR_VOID grCmdUpdateMemory(
     GR_GPU_SIZE dataSize,
     const GR_UINT32* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdFillMemory(
@@ -474,7 +474,7 @@ GR_VOID grCmdFillMemory(
     GR_GPU_SIZE fillSize,
     GR_UINT32 data)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdClearDepthStencil(
@@ -485,21 +485,21 @@ GR_VOID grCmdClearDepthStencil(
     GR_UINT rangeCount,
     const GR_IMAGE_SUBRESOURCE_RANGE* pRanges)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdSetEvent(
     GR_CMD_BUFFER cmdBuffer,
     GR_EVENT event)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdResetEvent(
     GR_CMD_BUFFER cmdBuffer,
     GR_EVENT event)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdMemoryAtomic(
@@ -509,7 +509,7 @@ GR_VOID grCmdMemoryAtomic(
     GR_UINT64 srcData,
     GR_ENUM atomicOp)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdBeginQuery(
@@ -518,7 +518,7 @@ GR_VOID grCmdBeginQuery(
     GR_UINT slot,
     GR_FLAGS flags)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdEndQuery(
@@ -526,7 +526,7 @@ GR_VOID grCmdEndQuery(
     GR_QUERY_POOL queryPool,
     GR_UINT slot)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdResetQueryPool(
@@ -535,7 +535,7 @@ GR_VOID grCmdResetQueryPool(
     GR_UINT startQuery,
     GR_UINT queryCount)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdWriteTimestamp(
@@ -544,7 +544,7 @@ GR_VOID grCmdWriteTimestamp(
     GR_GPU_MEMORY destMem,
     GR_GPU_SIZE destOffset)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdInitAtomicCounters(
@@ -554,7 +554,7 @@ GR_VOID grCmdInitAtomicCounters(
     GR_UINT counterCount,
     const GR_UINT32* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdLoadAtomicCounters(
@@ -565,7 +565,7 @@ GR_VOID grCmdLoadAtomicCounters(
     GR_GPU_MEMORY srcMem,
     GR_GPU_SIZE srcOffset)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID grCmdSaveAtomicCounters(
@@ -576,7 +576,7 @@ GR_VOID grCmdSaveAtomicCounters(
     GR_GPU_MEMORY destMem,
     GR_GPU_SIZE destOffset)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 // Debug Functions
@@ -585,7 +585,7 @@ GR_RESULT GR_STDCALL grDbgSetValidationLevel(
     GR_DEVICE device,
     GR_ENUM validationLevel)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -593,14 +593,14 @@ GR_RESULT grDbgRegisterMsgCallback(
     GR_DBG_MSG_CALLBACK_FUNCTION pfnMsgCallback,
     GR_VOID* pUserData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grDbgUnregisterMsgCallback(
     GR_DBG_MSG_CALLBACK_FUNCTION pfnMsgCallback)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -609,7 +609,7 @@ GR_RESULT grDbgSetMessageFilter(
     GR_ENUM msgCode,
     GR_ENUM filter)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -618,7 +618,7 @@ GR_RESULT grDbgSetObjectTag(
     GR_SIZE tagSize,
     const GR_VOID* pTag)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -627,7 +627,7 @@ GR_RESULT grDbgSetGlobalOption(
     GR_SIZE dataSize,
     const GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -637,7 +637,7 @@ GR_RESULT grDbgSetDeviceOption(
     GR_SIZE dataSize,
     const GR_VOID* pData)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -645,13 +645,13 @@ GR_VOID GR_STDCALL grCmdDbgMarkerBegin(
     GR_CMD_BUFFER cmdBuffer,
     const GR_CHAR* pMarker)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 GR_VOID GR_STDCALL grCmdDbgMarkerEnd(
     GR_CMD_BUFFER cmdBuffer)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
 }
 
 // WSI Functions
@@ -661,7 +661,7 @@ GR_RESULT grWsiWinGetDisplays(
     GR_UINT* pDisplayCount,
     GR_WSI_WIN_DISPLAY* pDisplayList)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -670,7 +670,7 @@ GR_RESULT grWsiWinGetDisplayModeList(
     GR_UINT* pDisplayModeCount,
     GR_WSI_WIN_DISPLAY_MODE* pDisplayModeList)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -678,14 +678,14 @@ GR_RESULT grWsiWinTakeFullscreenOwnership(
     GR_WSI_WIN_DISPLAY display,
     GR_IMAGE image)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grWsiWinReleaseFullscreenOwnership(
     GR_WSI_WIN_DISPLAY display)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -693,14 +693,14 @@ GR_RESULT grWsiWinSetGammaRamp(
     GR_WSI_WIN_DISPLAY display,
     const GR_WSI_WIN_GAMMA_RAMP* pGammaRamp)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
 GR_RESULT grWsiWinWaitForVerticalBlank(
     GR_WSI_WIN_DISPLAY display)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -708,7 +708,7 @@ GR_RESULT grWsiWinGetScanLine(
     GR_WSI_WIN_DISPLAY display,
     GR_INT* pScanLine)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
 
@@ -716,6 +716,6 @@ GR_RESULT grWsiWinSetMaxQueuedFrames(
     GR_DEVICE device,
     GR_UINT maxFrames)
 {
-    printf("STUB: %s\n", __func__);
+    LOGW("STUB\n");
     return GR_UNSUPPORTED;
 }
