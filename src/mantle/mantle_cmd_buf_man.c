b3462a87334ba99f2e38b2b279e1c6f96382b57f
xantares <xantares09@hotmail.com>
2021-03-15 17:53:00 +0100

    fix -Wint-conversion

--- src/mantle/mantle_cmd_buf_man.c
+++ src/mantle/mantle_cmd_buf_man.c
@@ -43,7 +43,7 @@ GR_RESULT grCreateCommandBuffer(
         .grPipeline = NULL,
         .grDescriptorSet = NULL,
         .attachmentCount = 0,
-        .attachments = { NULL },
+        .attachments = { VK_NULL_HANDLE },
         .minExtent2D = { 0, 0 },
         .minLayerCount = 0,
         .hasActiveRenderPass = false,
