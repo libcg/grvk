a5479235e3ed8d73c54a60a234affa8a641b6297
Clément Guérin <libcg@protonmail.com>
2022-07-18 21:07:00 -0700

    amdilc.exe: support compiling multiple shaders

--- src/amdilc/main.c
+++ src/amdilc/main.c
@@ -10,31 +10,36 @@ int main(int argc, char* argv[])
     logInit("", "");
 
     if (argc < 2) {
-        LOGE("%s [IL binary]\n", argv[0]);
+        LOGE("GRVK's amdilc -> SPIR-V offline compiler\n");
+        LOGE("usage: GRVK_DUMP_SHADERS=1 %s [IL binary] ...\n", argv[0]);
         return 1;
     }
 
-    FILE* file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        LOGE("failed to open %s\n", argv[1]);
-        return 1;
-    }
-
-    if (getenv("GRVK_DUMP_SHADERS") == NULL) {
+    if (getenv("GRVK_DUMP_SHADERS") == NULL || strcmp(getenv("GRVK_DUMP_SHADERS"), "1") != 0) {
         LOGW("GRVK_DUMP_SHADERS isn't set. Logs only.\n");
     }
 
-    fseek(file, 0, SEEK_END);
-    int size = ftell(file);
-    fseek(file, 0, SEEK_SET);
+    for (unsigned i = 1; i < argc; i++) {
+        LOGW("compiling %s... (%d/%d)\n", argv[i], i, argc - 1);
 
-    void* data = malloc(size);
-    fread(data, 1, size, file);
+        FILE* file = fopen(argv[i], "rb");
+        if (file == NULL) {
+            LOGE("failed to open %s\n", argv[i]);
+            return 1;
+        }
 
-    ilcCompileShader(data, size);
+        fseek(file, 0, SEEK_END);
+        int size = ftell(file);
+        fseek(file, 0, SEEK_SET);
 
-    free(data);
-    fclose(file);
+        void* data = malloc(size);
+        fread(data, 1, size, file);
+        fclose(file);
+
+        ilcCompileShader(data, size);
+
+        free(data);
+    }
 
     return 0;
 }
